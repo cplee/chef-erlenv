5f34e7aa453cf8164368e4c31d9bef5a601c41a5
alisdair sullivan <alisdairsullivan@yahoo.ca>
2013-04-23 10:43:18 -0700

    a minimal chef recipe for installing erlenv on ubuntu/debian

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name "erlenv"
+maintainer "alisdair sullivan"
+maintainer_email "alisdairsullivan@yahoo.ca"
+license "MIT"
+description "a chef lwrp for installing erlenv"
+version "0.1"
+
+depends "git"
+
+supports "ubuntu"
+supports "debian"
